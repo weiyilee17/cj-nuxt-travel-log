9b256a290394b93611c66f414cc91863e3e8edfd
Weiyi Lee <weiyilee17@gmail.com>
2026-08-22 11:28:15 +0800

    Add landing page

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink class="btn btn-ghost text-xl" to="/ ">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <button class="btn btn-accent">
+        Sign In With Github
+        <Icon name="tabler:brand-github" size="24" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures with this simple travel log
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Github
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
