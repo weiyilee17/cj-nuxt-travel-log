46ebb4329a3ff3db490b3c4476bc066169e15b17
Weiyi Lee <weiyilee17@gmail.com>
2026-08-27 12:58:55 +0800

    Add validation for env variables (#4)
    * Add validation for env variables
    
    * Remove test banana env variable

---
+++ lib/env.ts
@@ -0,0 +1,16 @@
+/// <reference types="node" />
+
+import { z } from "zod";
+
+import tryParseEnv from "./try-parse-env";
+
+const envSchema = z.object({
+  NODE_ENV: z.string(),
+});
+
+export type EnvSchema = z.infer<typeof envSchema>;
+
+tryParseEnv(envSchema);
+
+// eslint-disable-next-line node/no-process-env
+export default envSchema.parse(process.env);
