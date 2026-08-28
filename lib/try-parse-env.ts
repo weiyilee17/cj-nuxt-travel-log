f12cc6f3a0fcd396c37423b4775fc4027c657942
Weiyi Lee <weiyilee17@gmail.com>
2026-08-28 13:19:54 +0800

    Fix typescript implicit symbol convert to string error

--- lib/try-parse-env.ts
+++ lib/try-parse-env.ts
@@ -16,7 +16,7 @@ export default function tryParseEnv<T extends ZodRawShape>(
     if (error instanceof ZodError) {
       let message = "Missing required values in .env:\n";
       error.issues.forEach((issue) => {
-        message += `${issue.path[0]}\n`;
+        message += `${issue.path[0]?.toString()}\n`;
       });
       const e = new Error(message);
       e.stack = "";
