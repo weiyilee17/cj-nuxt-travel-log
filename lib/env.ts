568d79d8e0f931580226af4caad9311c76bea628
Weiyi Lee <weiyilee17@gmail.com>
2026-09-01 13:51:56 +0800

    Add missing BETTER_AUTH_URL env variable

--- lib/env.ts
+++ lib/env.ts
@@ -9,6 +9,7 @@ const envSchema = z.object({
   TURSO_DATABASE_URL: z.string(),
   TURSO_AUTH_TOKEN: z.string(),
   BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof envSchema>;
