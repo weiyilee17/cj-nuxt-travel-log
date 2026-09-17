8f796800e9d9cde63c44036d9c62968d59e79c35
Weiyi Lee <weiyilee17@gmail.com>
2026-09-17 22:40:58 +0800

    Add sign out button

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -3,7 +3,34 @@ const authStore = useAuthStore();
 </script>
 
 <template>
+  <details
+    v-if="!authStore.loading && authStore.user"
+    class="dropdown dropdown-end"
+  >
+    <summary class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-7 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </summary>
+    <!-- TODO: check why we need the text-base-content class to make the sign out text correct in light mode -->
+    <ul
+      class="menu dropdown-content bg-base-200 rounded-box z-1 w-48 p-2 shadow-sm text-base-content"
+    >
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </details>
+
+  <!-- TODO: check all the colors in light mode in general, the loading color doesn't feel right -->
   <button
+    v-else
     class="btn btn-accent"
     :disabled="authStore.loading"
     @click="authStore.signIn"
--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,24 +1,33 @@
-import { createAuthClient } from "better-auth/client";
+import { createAuthClient } from "better-auth/vue";
 
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("authStore", () => {
-  const loading = ref(false);
+  const session = authClient.useSession();
+
+  const user = computed(() => session.value.data?.user);
+
+  const loading = computed(
+    () => session.value.isPending || session.value.isPending,
+  );
 
   async function signIn() {
-    loading.value = true;
-
     await authClient.signIn.social({
       provider: "github",
       callbackURL: "/dashboard",
       errorCallbackURL: "/error",
     });
+  }
 
-    loading.value = false;
+  async function signOut() {
+    await authClient.signOut();
+    navigateTo("/");
   }
 
   return {
     loading,
     signIn,
+    signOut,
+    user,
   };
 });
